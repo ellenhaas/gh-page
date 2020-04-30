864be214130a5f87ae5bcb73272d4721cac09185
nouninoun <haas.ellen@courrier.uqam.ca>
2020-04-30 17:53:46 -0400

    Updates

---
+++ precache-manifest.1947f2022f9fd8ec2a1261b4266cb2d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4cf40f371a76b2fffa13cfa251aa3d",
+    "url": "/gh-page/index.html"
+  },
+  {
+    "revision": "65ab72c8288d387686b2",
+    "url": "/gh-page/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "18757a2631844cfd1255",
+    "url": "/gh-page/static/css/main.a97ab7fe.chunk.css"
+  },
+  {
+    "revision": "65ab72c8288d387686b2",
+    "url": "/gh-page/static/js/2.2ba1505e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gh-page/static/js/2.2ba1505e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18757a2631844cfd1255",
+    "url": "/gh-page/static/js/main.d1bede8e.chunk.js"
+  },
+  {
+    "revision": "29827eb4956bd2769cf4",
+    "url": "/gh-page/static/js/runtime-main.da820854.js"
+  },
+  {
+    "revision": "14127c52932d7c525f885a591e9f2494",
+    "url": "/gh-page/static/media/avatar.14127c52.png"
+  },
+  {
+    "revision": "6c84d938c0ed43562af9a819bf4452aa",
+    "url": "/gh-page/static/media/backg_code.6c84d938.png"
+  }
+]);
