46193f122d2c5a0fa499d77174be88f66f9c93c5
nouninoun <haas.ellen@courrier.uqam.ca>
2020-05-01 15:12:27 -0400

    techs complete

--- src/components/techs.js
+++ src/components/techs.js
@@ -4,8 +4,8 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab, faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { fas, faCheckCircle, faLaptopCode, faIdCard } from '@fortawesome/free-solid-svg-icons'
+import { fab, faPhp, faJava, faJsSquare, faHtml5, faCss3Alt, faReact, faVuejs, faSymfony, faBootstrap, faAngular, faGitAlt, faMarkdown, faNodeJs, faTrello, faUbuntu, faWindows, faLinux, faWordpress} from '@fortawesome/free-brands-svg-icons'
+import { fas, faCheckCircle, faLaptopCode, faIdCard, faDatabase } from '@fortawesome/free-solid-svg-icons'
 
 library.add(
     fab,
@@ -50,13 +50,80 @@ export default class Techs extends Component {
                     </Container>
                     <br /> <hr/><br />
                     <Container>
-                    <span className="fa-container-inline"><FontAwesomeIcon className="fa-icon-vert" icon="" size="2x"/></span><br/>
-                    <span className="fa-container-inline"><FontAwesomeIcon className="fa-icon-vert" icon="" size="2x"/></span><br/>
-                    <span className="fa-container-inline"><FontAwesomeIcon className="fa-icon-vert" icon="" size="2x"/></span><br/>
-                    <span className="fa-container-inline"><FontAwesomeIcon className="fa-icon-vert" icon="" size="2x"/></span><br/>
-                    <span className="fa-container-inline"><FontAwesomeIcon className="fa-icon-vert" icon="" size="2x"/></span><br/>
-                    <span className="fa-container-inline"><FontAwesomeIcon className="fa-icon-vert" icon="" size="2x"/></span><br/>
-                    <span className="fa-container-inline"><FontAwesomeIcon className="fa-icon-vert" icon="" size="2x"/></span><br/>
+                        <h3>Languages/Framework</h3>
+                        <Row>
+                            <Col xs={{span: 4}} md={{span: 1}} >
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faPhp} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faSymfony} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faBootstrap} size="2x"/>
+                            </Col>
+
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faHtml5} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faCss3Alt} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faMarkdown} size="2x"/>
+                            </Col>
+                            
+                            <Col xs={{span: 4}} md={{span: 1}} >
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faJsSquare} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faReact} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faVuejs} size="2x"/>
+                            </Col>
+
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faNodeJs} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faAngular} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faJava} size="2x"/>
+                            </Col>
+                        </Row>
+
+
+                        <hr/>
+                        <h3>Outils</h3>
+                        <Row>
+                            <Col xs={{span: 4}} md={{span: 1}} >
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faGitAlt} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faTrello} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faDatabase} size="2x"/>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={{span: 4}} md={{span: 1}} >
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faLinux} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faUbuntu} size="2x"/>
+                            </Col>
+                            <Col xs={{span: 4}} md={{span: 1}}>
+                            <FontAwesomeIcon className="fa-icon-vert" icon={faWindows} size="2x"/>
+                            </Col>
+                        </Row>
+                        <br/>
+                        <Row>
+                            <Col xs={{span: 10, offset: 1}} md={{span: 10, offset: 1}}>
+                            <h5>Sélénium, WAMP/LAMP, MySQL, Oracle server, Maven, Travis CI, Agile, Scrum...</h5>
+                            </Col>
+                        </Row>
                     </Container>
                 </div>
             </section>
